b091e39567bca364bd370fbf22ae8680b2e9dc18
Альфред <lenq448@gmail.com>
2026-04-09 10:37:05 +0500

    Add early return handling in main

--- library-management-system/src/utils.py
+++ library-management-system/src/utils.py
@@ -1,4 +1,7 @@
 ﻿def calculate_fine(days_overdue):
+    # Alternative: no fine if returned early
+    if days_overdue <= 0:
+        return 0
     return days_overdue * 0.5
 
 def format_book_title(title):
